b0552cf5a93a0f194033d44a26432f11f6b25c64
kevinj37 <kevinxhajiang@gmail.com>
2026-04-04 12:50:44 -0400

    Feat: Fixed project card overflow on mobile screens. Added more project cards

---
+++ kevin_react/src/interfaces/projectCard.ts
@@ -0,0 +1,12 @@
+export interface ProjectCardProps {
+    title: string;
+    description?: string;
+    date?: string;
+    linkTo?: string;
+    imageLinks?: string[];
+    videoLink?: string;
+    attachDocument?: string;
+    backgroundColour: string;
+    titleColour: string;
+    
+}
