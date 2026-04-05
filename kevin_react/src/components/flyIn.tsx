c21451fad32f741681fed4cd1911416c4bc06227
kevinj37 <kevinxhajiang@gmail.com>
2026-04-04 23:31:32 -0400

    Feat: Finished adding clouds to background of project cards

---
+++ kevin_react/src/components/flyIn.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef, useState } from "react";
+
+export function useFlyIn(threshold = 0.2, runOnce = false) {
+  const flyInRef = useRef<HTMLDivElement | null>(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const element = flyInRef.current;
+    if (!element) return;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (runOnce) {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+            observer.unobserve(element);
+          }
+        }
+        else {
+          setIsVisible(entry.isIntersecting)
+        }
+      },
+      { threshold }
+    );
+
+    observer.observe(element);
+    return () => observer.disconnect();
+  }, [threshold]);
+
+  return {flyInRef, isVisible};
+}
--- kevin_react/src/components/projectCard.tsx
+++ kevin_react/src/components/projectCard.tsx
@@ -1,11 +1,18 @@
 import type { ProjectCardProps } from "@/interfaces/projectCard";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { useFlyIn } from "./flyIn";
 import "@/App.css"
 
+
 export default function ProjectCard({title, description, date, linkTo, imageLinks, videoLink, attachDocument, backgroundColour, titleColour }: ProjectCardProps) {
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
   const [nextIndex, setNextIndex] = useState(1);
   const [isFading, setIsFading] = useState(false);
+  const distanceX = (Math.random() * -180 - 90) * (Math.round(Math.random()) * -2 + 1);
+  const distanceY = (Math.random() - 0.5) * 150;
+  const hoverType = Math.floor(Math.random() * 3)
+
+  const {flyInRef, isVisible} = useFlyIn();
 
   useEffect(() => {
     if (imageLinks?.length) {
@@ -28,62 +35,67 @@ export default function ProjectCard({title, description, date, linkTo, imageLink
     
     return (
         <>
-        <div className="projectCloud logo">
-        <p className="projectCardDate" style={{color: titleColour}}><b>{date}</b></p>
-        <div className="projectCard" style={{backgroundColor: backgroundColour}}>
-            {linkTo && 
-            <a href={linkTo}>
-                <div className="projectCardTitle" style={{backgroundColor: titleColour}}>
+        <div ref={flyInRef} className={`cloudFlyIn ${isVisible ? "show" : ""}`}
+        style={{ "--distanceX": `${distanceX}px`,
+                 "--distanceY": `${distanceY}px`} as React.CSSProperties}>
+            <div className={`projectCloud logo 
+                ${hoverType == 0 ? "projectCloudHover" : (hoverType == 1 ? "projectCloudHover2" : "projectCloudHover3")}`}>
+            <p className="projectCardDate" style={{color: titleColour}}><b>{date}</b></p>
+            <div className="projectCard" style={{backgroundColor: backgroundColour}}>
+                {linkTo && 
+                <a href={linkTo}>
+                    <div className="projectCardTitle" style={{backgroundColor: titleColour}}>
+                        <div className="innerProjectCardTitle">
+                            <h3>{title}</h3>
+                        </div>
+                    </div>
+                </a>}
+                {!linkTo && 
+                <div className="projectCardTitleNoLink" style={{backgroundColor: titleColour}}>
                     <div className="innerProjectCardTitle">
                         <h3>{title}</h3>
                     </div>
                 </div>
-            </a>}
-            {!linkTo && 
-            <div className="projectCardTitleNoLink" style={{backgroundColor: titleColour}}>
-                <div className="innerProjectCardTitle">
-                    <h3>{title}</h3>
-                </div>
+                }
+                <p>{description}</p>
+                {imageLinks?.length && 
+                <div>
+                    <div className="projectImageContainer" onClick={() => window.location.href = imageLinks[currentIndex]}>
+                        <img src={imageLinks[currentIndex]} 
+                        className="projectImage baseImage"
+                        alt={imageLinks[nextIndex]}>
+                        </img>
+                        <img src={imageLinks[nextIndex]} 
+                        className={`projectImage overlayImage ${isFading? "active" : ""}`}>
+                        </img>
+                    </div>
+                </div>}
+                {videoLink && 
+                <div className="baseVideo">
+                    <video src={videoLink} 
+                    onClick={() => window.location.href = videoLink}
+                    autoPlay muted loop>
+                    </video>
+                </div>}
+                {attachDocument && 
+                <div style={{ position: "relative" }}>
+                    <embed
+                        src={`${attachDocument}#zoom=page-width`}
+                        type="application/pdf"
+                        className="projectDocument"
+                    />
+                    <div className="documentLinkPadding"
+                    style={{backgroundColor: titleColour}}>
+                        <a href={attachDocument}>📖 View Document</a>
+                    </div>
+                </div>}
+                <br />
+                {linkTo && <p style={{ 
+                    fontSize: "14px", overflow: "hidden", 
+                    textOverflow: "ellipsis", whiteSpace: "nowrap",
+                    maxWidth: 200, marginLeft: "auto"}}><a href={linkTo}>🔗 {linkTo}</a></p>}
             </div>
-            }
-            <p>{description}</p>
-            {imageLinks?.length && 
-            <div>
-                <div className="projectImageContainer" onClick={() => window.location.href = imageLinks[currentIndex]}>
-                    <img src={imageLinks[currentIndex]} 
-                    className="projectImage baseImage"
-                    alt={imageLinks[nextIndex]}>
-                    </img>
-                    <img src={imageLinks[nextIndex]} 
-                    className={`projectImage overlayImage ${isFading? "active" : ""}`}>
-                    </img>
-                </div>
-            </div>}
-            {videoLink && 
-            <div className="baseVideo">
-                <video src={videoLink} 
-                onClick={() => window.location.href = videoLink}
-                autoPlay muted loop>
-                </video>
-            </div>}
-            {attachDocument && 
-            <div style={{ position: "relative" }}>
-                <embed
-                    src={`${attachDocument}#zoom=page-width`}
-                    type="application/pdf"
-                    className="projectDocument"
-                />
-                <div className="documentLinkPadding"
-                style={{backgroundColor: titleColour}}>
-                    <a href={attachDocument}>📖 View Document</a>
-                </div>
-            </div>}
-            <br />
-            {linkTo && <p style={{ 
-                fontSize: "14px", overflow: "hidden", 
-                textOverflow: "ellipsis", whiteSpace: "nowrap",
-                maxWidth: 200, marginLeft: "auto"}}><a href={linkTo}>🔗 {linkTo}</a></p>}
-        </div>
+            </div>
         </div>
         </>
     )
