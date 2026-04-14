4d5b5e0676b7984761e8cc9c4a626ad183cd9007
Kevin Jiang <kevinxhjiang@gmail.com>
2026-04-14 10:36:00 -0400

    Chore: Refactored bg clouds css for more versatility + readability

--- kevin_react/src/components/backgroundCloud.tsx
+++ kevin_react/src/components/backgroundCloud.tsx
@@ -3,15 +3,15 @@
 export default function BackgroundClouds() {
     return (
         <>
-        <div className="bg-cloud1"></div>
-        <div className="bg-cloud2"></div>
-        <div className="bg-cloud3"></div>
-        <div className="bg-cloud4"></div>
-        <div className="bg-cloud5"></div>
-        <div className="bg-cloud6"></div>
-        <div className="bg-cloud7"></div>
-        <div className="bg-cloud8"></div>
-        <div className="bg-cloud9"></div>
+        <div className="bg-cloud bg-cloud1"></div>
+        <div className="bg-cloud bg-cloud2"></div>
+        <div className="bg-cloud bg-cloud3"></div>
+        <div className="bg-cloud bg-cloud4"></div>
+        <div className="bg-cloud bg-cloud5"></div>
+        <div className="bg-cloud bg-cloud6"></div>
+        <div className="bg-cloud bg-cloud7"></div>
+        <div className="bg-cloud bg-cloud8"></div>
+        <div className="bg-cloud bg-cloud9"></div>
         </>
     )
 }
